5e93bdf0471dce730491f09aabdaaa33804aca42
Yurii Vynar <yurik.vinar37@gmail.com>
2022-08-31 09:29:56 +0300

    3.1

--- js/menu.js
+++ js/menu.js
@@ -1,24 +1,22 @@
 (() => {
-    const refs = {
-      openMenuBtn: document.querySelector('.mobile__button'),
-      closeMenuBtn: document.querySelector('.mobile__button-close'),
-      menu: document.querySelector('.mobile'),
-      body: document.querySelector('body'),
-      menuList: document.querySelector('.mobile__nav'),
-    };
-  
-    refs.openMenuBtn.addEventListener('click', toggleMenu);
-    refs.closeMenuBtn.addEventListener('click', toggleMenu);
-    refs.menuList.addEventListener('click', removeMenu);
-  
-    function toggleMenu() {
-      refs.menu.classList.toggle('is-add');
-      refs.menu.classList.toggle('is-hiddenn');
-      refs.body.classList.toggle('no-scroll');
-    }
-  
-    function removeMenu() {
-      refs.menu.classList.add('is-hiddenn');
-      refs.menu.classList.add('is-add');
-    }
-  })();
+  const refs = {
+    openMenuBtn: document.querySelector('.mobile__button'),
+    closeMenuBtn: document.querySelector('.mobile__icon-close'),
+    menu: document.querySelector('.mobile'),
+    body: document.querySelector('body'),
+    menuList: document.querySelector('.mobile__nav'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.menuList.addEventListener('click', removeMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+
+  function removeMenu() {
+    refs.menu.classList.add('is-hidden');
+  }
+})();
